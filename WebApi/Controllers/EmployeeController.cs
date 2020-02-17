63f6f3a20aee48a5ff82e09855aa126674a42f84
jammishah <jammishah@gmail.com>
2020-02-17 16:09:29 +0530

    addin files

---
+++ WebApi/Controllers/EmployeeController.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using EmployeeClass;
+
+namespace WebApi.Controllers
+{
+    public class EmployeeController : ApiController
+    {
+        [HttpGet]
+        public List<employee> Get()
+
+        {
+
+            using (EmployeeDBEntities entity = new EmployeeDBEntities())
+            {
+
+
+                return entity.employees.ToList();
+               
+
+            }
+
+
+
+
+        }
+        [HttpGet]
+        
+        public employee Get(int id)
+
+        {
+            using (EmployeeDBEntities entity = new EmployeeDBEntities())
+
+            {
+                return entity.employees.FirstOrDefault(x => x.UserID == id);
+
+            }
+        }
+        [HttpPost]
+        public void Post([FromBody]employee value)
+        {
+            using (EmployeeDBEntities entity = new EmployeeDBEntities())
+            {
+                var emp = new employee()
+
+                {
+                    Name = value.Name,
+                    Designation = value.Designation,
+                    UserID = value.UserID,
+                    Department = value.Department
+                };
+                entity.employees.Add(emp);
+                entity.SaveChanges();
+            }
+        }
+
+        [HttpPut]
+        public void Put(int id, [FromBody]employee value)
+        {
+            using (EmployeeDBEntities entity = new EmployeeDBEntities())
+            {
+                var ent = entity.employees.FirstOrDefault(x => x.UserID == id);
+
+                ent.Department = value.Department;
+                ent.Designation = value.Designation;
+                ent.Name = value.Name;
+
+                entity.SaveChanges();
+            }
+
+        }
+        [HttpDelete]
+        public void Delete (int id)
+        {
+            using (EmployeeDBEntities ABV = new EmployeeDBEntities())
+            {
+                ABV.employees.Remove(ABV.employees.FirstOrDefault(x => x.UserID == id));
+
+                ABV.SaveChanges();
+
+            }
+
+
+        }
+    }
+
+}
---
+++ WebApi/Controllers/ValuesController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using EmployeeClass;
+namespace WebApi.Controllers
+{
+    // [Authorize]
+    public class ValuesController : ApiController
+    {
+        [HttpGet]
+        // GET api/values
+        public IEnumerable<employee> Get()
+        {
+            using (EmployeeDBEntities ABC = new EmployeeDBEntities())
+                return ABC.employees.ToList();
+        }
+        [HttpGet]
+        // GET api/values/5
+        public IEnumerable<employee> Get(int id)
+        {
+            using (EmployeeDBEntities ABC = new EmployeeDBEntities())
+                yield return ABC.employees.FirstOrDefault(x => x.UserID == id);
+        }
+        [HttpPost]
+        // POST api/values
+        public void Post([FromBody]employee value)
+        {
+            using (EmployeeDBEntities ABC = new EmployeeDBEntities())
+            {
+                ABC.employees.Add(value);
+                ABC.SaveChanges();
+            }
+        }
+        // PUT api/values/5
+        [HttpPut]
+        public void Put(int id, [FromBody]employee value)
+        {
+            //using (EmployeeDBEntities ABC = new EmployeeDBEntities())
+            //{
+            //    ABC.employees.a(value);
+            //    abc.savechanges();
+            //}
+        }
+        [HttpDelete]
+        // DELETE api/values/5
+        public void Delete(int value)
+        {
+            using (EmployeeDBEntities ABC = new EmployeeDBEntities())
+            {
+                ABC.employees.Remove(ABC.employees.FirstOrDefault(x => x.UserID == value));
+                ABC.SaveChanges();
+            }
+        }
+    }
+}
